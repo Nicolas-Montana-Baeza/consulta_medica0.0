5109bdbfca96c2765b78c0174437b29c6b592e23
nicolas <ni.cco896@gmail.com>
2021-05-02 17:52:38 -0400

    jeje

--- medico.py
+++ medico.py
@@ -31,7 +31,24 @@ class Medico(Persona):
     def recetarPaciente(self, _receta,_paciente):
         _receta=_paciente.getRecetas.append(_receta)
         _paciente.set(_receta)
-       
+
+    def requerirExamen(self, _examen, _paciente):
+        _examen=_paciente.getRequerimientos().append(_examen)
+        _paciente.setRequerimientos(_examen)
+        return _paciente
+
+
+    def isDisponible(self, _fecha):
+        for disponible in self.disponibilidad:
+            if _fecha==disponible: 
+                return True
+        return False
+
+    def diagnosticarPaciente(self, _diagnostico, _paciente):
+        _diagnostico=_paciente.getDiagnosticos().append(_diagnostico)
+        _paciente.setDiagnosticos(_diagnostico)
+        return _paciente
+
     def __str__(self) :
         return str(self.disponibilidad)+" "+self.especialidad
 
