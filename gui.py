75ed81369291fd8f9c349c2e9561236334d495e3
nicolas <ni.cco896@gmail.com>
2021-05-02 20:35:22 -0400

    jej

--- gui.py
+++ gui.py
@@ -9,5 +9,4 @@ ventana_principal.geometry("1300x568")
 fondo_imagen= PhotoImage(file="/home/nicolas/Documents/consulta_medica0.0/imagenes/plantilla_gris.png")
 fondo= Label(ventana_principal, image=fondo_imagen)
 fondo.place(x=0,y=0,relwidth=1, relheight=1)
-
 ventana_principal.mainloop()
