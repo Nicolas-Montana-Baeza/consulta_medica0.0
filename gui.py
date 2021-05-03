1064866c623a88d45e1e40d38f4b25764e87d5af
nicolas <ni.cco896@gmail.com>
2021-05-02 20:42:53 -0400

    jeje

--- gui.py
+++ gui.py
@@ -9,4 +9,6 @@ ventana_principal.geometry("1300x568")
 fondo_imagen= PhotoImage(file="/home/nicolas/Documents/consulta_medica0.0/imagenes/plantilla_gris.png")
 fondo= Label(ventana_principal, image=fondo_imagen)
 fondo.place(x=0,y=0,relwidth=1, relheight=1)
+RADIOBUTTON(ventana_principal, text=("Kinesiologia"), variable= "Kinesiologia", value=1, bg="#788890").pack()
+
 ventana_principal.mainloop()
