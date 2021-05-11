dae5514daf270c783c414f748ae5976414c3ee53
Maiteeeeee <m.santzcruz.p02@gmail.com>
2021-05-11 06:32:43 -0400

    dfg

--- gui.py
+++ gui.py
@@ -102,7 +102,7 @@ def confirmarCita():
         for cita in paciente.getCitas():
             if cita.getCodigo()==busqueda:
                 info_cita_txtbox.delete('0.0',END)
-                cita.setConfirmar(True)
+                cita.setConfirmada(True)
                 texto= "Fecha Citada: "+str(cita.getFechaCitada())+"\nPaciente: "+ cita.getPaciente().getNombreCompleto()+"\nMedico: "+cita.getMedico().getNombreCompleto()+"\nPrestacion: "+cita.getPrestacion()+"\nModalidad: "+cita.getModalidad()+"\nConfirmada: "+str(cita.getConfirmada())
                 info_cita_txtbox.insert('0.0',texto)
                 return True
