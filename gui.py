fd2a316d1765ee59849662f52fb2ad9b4c23077b
nicolas <ni.cco896@gmail.com>
2021-05-11 05:37:24 -0400

    dzczx

--- gui.py
+++ gui.py
@@ -175,8 +175,8 @@ def pantallaCodigo(cita_auxiliar):
     temp=Tk()
     label=Label(temp,text="La cita a sido agendada con exito\nGuarde el siguiente codigo para administrar su cita").pack()
     entry=Entry(temp,state="readonly")
-    entry.insert(0,cita_auxiliar.getCodigo())
     entry.pack()
+    entry.insert(0,cita_auxiliar.getCodigo())
     label2=Label(temp,text="RECUERDE CONFIRMAR SU CITA O NO SERÁ ATENDIDO").pack()
     temp.mainloop()
 #función para agendar la cita
