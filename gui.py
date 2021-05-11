3021bb65a6acb824065590fd1b778935595c3494
nicolas <ni.cco896@gmail.com>
2021-05-11 06:13:36 -0400

    ñhussas

--- gui.py
+++ gui.py
@@ -110,7 +110,7 @@ def buscarCodigo():
         print("entro aca")
         for cita in paciente.getCitas():
             print("entro aca1")
-            if cita.getCodigo==busqueda:
+            if cita.getCodigo()==busqueda:
                 print("entro aca2")
                 texto= "Fecha Citada: "+cita.getFechaCitada()+"\nPaciente: "+ cita.getPaciente().getNombreCompleto()+"\nMedico: "+cita.getMedico()+"\nPrestacion: "+cita.getPrestacion()+"\nModalidad: "+cita.getModalidad()+"\nConfirmada: "+cita.getConfirmada()
 
