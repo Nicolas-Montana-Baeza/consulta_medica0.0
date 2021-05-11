c30747b9c64e606508a9026bd9b94c1ce4f42b04
nicolas <ni.cco896@gmail.com>
2021-05-11 06:27:59 -0400

    kjgjassaldaa

--- gui.py
+++ gui.py
@@ -96,11 +96,17 @@ def reagendarCita():
 
 #función para confirmar la cita
 def confirmarCita():
+
     busqueda= ingresar_codigo_entry.get()
     for paciente in clinica_objeto.getPacientes():
         for cita in paciente.getCitas():
-            return paciente.buscarCita(busqueda)
-    return
+            if cita.getCodigo()==busqueda:
+                info_cita_txtbox.delete('0.0',END)
+                cita.setConfirmar(True)
+                texto= "Fecha Citada: "+str(cita.getFechaCitada())+"\nPaciente: "+ cita.getPaciente().getNombreCompleto()+"\nMedico: "+cita.getMedico().getNombreCompleto()+"\nPrestacion: "+cita.getPrestacion()+"\nModalidad: "+cita.getModalidad()+"\nConfirmada: "+str(cita.getConfirmada())
+                info_cita_txtbox.insert('0.0',texto)
+                return True
+    return False
 
 #buscar cita por codigo
 def buscarCodigo():
