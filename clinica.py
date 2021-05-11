39b2e4ae4bcbc431d8bd346e04de0cce1c8a0a9a
nicolas <ni.cco896@gmail.com>
2021-05-10 20:44:17 -0400

    qsada

--- clinica.py
+++ clinica.py
@@ -347,21 +347,35 @@ class Persona():
             return False
     
     def buscarCita(self,buscar):
-
         for cita in self.citas:
-
             if cita.getCodigo()==buscar:
                 return cita
 
-    def agendarCita(self,_cita):
-        
-        self.citas.append(_cita)
+    def agendarCita(self, _agendar):
+        for cita in self.citas:
+            if cita.getCodigo()==_agendar.getCodigo():
+                return False
+        self.citas.append(_agendar)
+        return True
 
-    def reagendarCita(self, _fecha, _codigo_cita):
-        return "not implemented yet"
+    def eliminarCita(self,_codigo):
+        for cita in self.citas:
+            if cita.getCodigo==_codigo:
+                self.citas.remove(cita)
+                return True
+        return False
     
-    def confirmarCita(self,_cita):
-        _cita.setConfirmada(True)
+    def modificarCita(self,fecha_citada,codigo):
+        for cita in self.citas:
+            if cita.getCodigo==codigo:
+                cita.setfechaCitada(fecha_citada)
+                return True
+        return False
+    
+    def confirmarCita(self,codigo):
+        for cita in self.citas:
+            if cita.getCodigo()==codigo:
+                cita.setConfirmada(True)
  
     def getNombreCompleto(self):
         return str(self.nombre1).title()+" "+str(self.nombre2).title()+" "+str(self.apellido1).title()+" "+str(self.apellido2).title()
